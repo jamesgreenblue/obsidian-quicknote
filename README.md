1b4928b5e5663a5a4be686b4c578b12771b934c7
James Greenhalgh <j@greenhalgh.io>
2023-08-08 22:43:17 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# obsidian-quicknote
-Create New note from right-clicking app icon
+# About
+
+Create a new quick note in a floating [Obsidian](https://obsidian.md) window (on command or by right-clicking the Obsidian app icon). This started from a [feature request](https://forum.obsidian.md/t/create-new-note-from-right-clicking-app-icon/63210) in the Obsidian forum.
+
+# TODO
+- [ ] Add a setting for changing the quick note title
+- [ ] Windows support (docs: [Windows Taskbar JumpList](https://www.electronjs.org/docs/latest/tutorial/windows-taskbar))
+- [ ] Linux support (docs: [Linux Desktop Launcher Actions](https://www.electronjs.org/docs/latest/tutorial/linux-desktop-actions))
+
+# Contributing
+
+Changes welcome! Please don't hesitate to open an issue if you spot a problem or propose an enhancement with a pull request. I don't have reliable access to a Windows or desktop Linux machine for testing so any help with the to-do items would be warmly received.
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-"use strict";
+// v0.1.0 https://github.com/jamesgreenblue/obsidian-quicknote is licensed under the MIT License
 
 const LOG_PREFIX = "obsidian-quicknote",
-  LOG_LOADING = "starting",
+  LOG_LOADING = "hello",
   LOG_CLEANUP = "bye bye",
   log = (message) => console.log(`${LOG_PREFIX}: ${message}`);
 
@@ -11,7 +11,7 @@ const obsidian = require("obsidian"),
 const dockMenu = Menu.buildFromTemplate([
 {
     label: 'New quick note',
-    click () { console.log('Hello World') }
+    click () { this.app.commands.executeCommandById('quicknote:jgb-create-quick-note'); }
 }
 ])
 
@@ -22,9 +22,32 @@ class QuickNotePlugin extends obsidian.Plugin {
     if (process.platform === 'darwin') {
         app.dock.setMenu(dockMenu)
     }
+
+    this.addCommand({
+      id: "jgb-create-quick-note",
+      name: "New quick note",
+      icon: "popup-open",
+      callback: async () => {
+        let fileName = this.generateNewFileFolderName();
+        let leafWindow = this.app.workspace.getLeaf("window");
+        const newFile = await this.app.vault.create(fileName, "", {});
+        await leafWindow.openFile(newFile);
+      }
+    });
   }
+
+  generateNewFileFolderName() {
+    let activeFile = this.app.workspace.getActiveFile();
+    let activePath = !!activeFile ? activeFile.path : "";
+    const newFileParent = this.app.fileManager.getNewFileParent(activePath);
+    let noteFilePath = newFileParent.path;
+    const noteName = obsidian.moment().format("YYYY-MM-DD [(]HH-mm-ss[) Quick note]");
+    return `${noteFilePath}/${noteName}.md`;
+  }
+
   onunload() {
     log(LOG_CLEANUP);
   }
+
 }
 module.exports = QuickNotePlugin;
